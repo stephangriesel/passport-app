438bc14859cc36a3e365b821b0cebd307e9d7a71
Stephan Griesel <sgriesel@gmail.com>
2018-12-30 16:37:19 +0100

    update models & encrypt passwords

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const router = express.Router();
+const bcrypt = require('bcryptjs');
+
+//User model
+const User = require('../models/User');
 
 // Login page
 router.get('/login', (req,res) => res.render('login'));
@@ -37,7 +41,41 @@ router.post('/register', (req, res) => {
             password2
         })
     } else {
-        res.send('pass');
+        // validation pass
+        User.findOne({ email: email })
+        .then(user => {
+            if(user) {
+                //User exists
+                error.push({ msg: 'Email is already registered' });
+                res.render('register',{
+                    errors,
+                    name,
+                    email,
+                    password,
+                    password2
+                })
+            } else {
+                const newUser = new User({
+                    name,
+                    email,
+                    password
+                });
+
+                // Hash password
+                bcrypt.genSalt(10, (err,salt) => 
+                    bcrypt.hash(newUser.password, salt, (err, hash) => {
+                        if(err) throw err;
+                        // Set password to hashed
+                        newUser.password = hash;
+                        // Save user
+                        newUser.save()
+                            .then(user => {
+                                res.redirect('/users/login')
+                            })
+                            .catch(err => console.log(err));
+                }))
+            }
+        });
     }
 });
 
