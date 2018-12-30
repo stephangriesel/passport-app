b50a99b323420c6369a1f16720be12775aa87c59
Stephan Griesel <sgriesel@gmail.com>
2018-12-30 12:22:51 +0100

    add views, updates routes, add models, update mongodb acccess

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
 const express = require('express');
-const expressLayouts = require('express-ejs-layouts')
+const expressLayouts = require('express-ejs-layouts');
+const mongoose = require('mongoose');
 
 const app = express();
+
+// Config
+const db = require('./config/keys').MongoURI;
+
+// Connect Mongo
+mongoose.connect(db, { useNewUrlParser:true })
+.then(() => console.log('MongoDB connected..'))
+.catch(err => console.log(err));
 
 // EJS
 app.use(expressLayouts);
