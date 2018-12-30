91c4a619f1626eb03830b80c5d76fc592d76d3c6
Stephan Griesel <sgriesel@gmail.com>
2018-12-30 18:43:37 +0100

    login handle

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcryptjs');
+const passport = require('passport');
 
 //User model
 const User = require('../models/User');
@@ -78,6 +79,15 @@ router.post('/register', (req, res) => {
             }
         });
     }
+});
+
+// Login handle
+router.post('/login', (req, res, next) => {
+    passport.authenticate('local', {
+        successRedirect: '/dashboard',
+        failureRedirect: '/users/login',
+        failureFlash: true
+    })(req, res, next);
 });
 
 module.exports = router;
